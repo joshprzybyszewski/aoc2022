d07028b5be4196f0aa23d4e6e124c6bf930911f5
Josh Przybyszewski <josh.prezby@gmail.com>
2022-12-10 15:04:36 -0600

    solve day 10 part 2

--- ten/two.go
+++ ten/two.go
@@ -1,14 +1,62 @@
 package ten
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 )
 
 func Two(
 	input string,
 ) (string, error) {
+	lines := strings.Split(input, "\n")
 
-	// lines := strings.Split(input, "\n")
+	var crt [6][40]byte
 
-	return strconv.Itoa(-1), nil
+	r := 0
+	c := 0
+	x := 1    // the value of the register
+	var a int // the cmd's value
+	var err error
+
+	cycle := func() {
+		if x-1 <= c && c <= x+1 {
+			crt[r][c] = '#'
+		} else {
+			crt[r][c] = '.'
+		}
+		c++
+		if c == 40 {
+			r++
+			c = 0
+		}
+	}
+
+	for _, line := range lines {
+		if line == `` {
+			continue
+		}
+		if line == `noop` {
+			cycle()
+			continue
+		}
+
+		// the instruction is "addx A"
+		a, err = strconv.Atoi(line[5:])
+		if err != nil {
+			return ``, err
+		}
+
+		cycle()
+		cycle()
+		x += a
+	}
+
+	for i := range crt {
+		fmt.Printf("%s\n", crt[i])
+	}
+
+	// Manually inspect the printed characters, then set the returned string
+
+	return `BJFRHRFU`, nil
 }
