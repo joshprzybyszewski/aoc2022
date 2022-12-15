0566c61e2fe927b981b795579233bc63ff1f85af
Josh Przybyszewski <josh.prezby@gmail.com>
2022-12-14 20:50:18 -0600

    solve part 2

--- thirteen/two.go
+++ thirteen/two.go
@@ -9,5 +9,58 @@ func Two(
 ) (int, error) {
 	lines := strings.Split(input, "\n")
 
-	return len(lines), nil
+	marker1 := []interface{}{
+		[]interface{}{
+			2,
+		},
+	}
+	marker2 := []interface{}{
+		[]interface{}{
+			6,
+		},
+	}
+
+	m1, m2 := 1, 1
+
+	var checkM1First bool
+	switch compare(marker1, marker2) {
+	case valid:
+		checkM1First = false
+		m2++
+	default:
+		checkM1First = true
+		m1++
+	}
+
+	for _, line := range lines {
+		if line == `` {
+			continue
+		}
+
+		l, _ := parse(line, 0)
+
+		if checkM1First {
+			switch compare(l, marker1) {
+			case valid:
+				m1++
+				switch compare(l, marker2) {
+				case valid:
+					m2++
+				}
+			}
+		} else {
+			switch compare(l, marker2) {
+			case valid:
+				m2++
+
+				switch compare(l, marker1) {
+				case valid:
+					m1++
+				}
+			}
+		}
+	}
+
+	// 12544, 12882 too low
+	return m1 * m2, nil
 }
