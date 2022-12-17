15c3646846e1beca8e4a1134697e3b5aace90772
Josh Przybyszewski <josh.prezby@gmail.com>
2022-12-17 09:13:21 -0600

    cut execution time in half

--- fifteen/two.go
+++ fifteen/two.go
@@ -12,19 +12,24 @@ func Two(
 		return 0, err
 	}
 
+	ts := newTuples()
+	var output []tuple
+
+	var x1, x2 int
+	var ok bool
+
 	const (
 		max = 4000000
 	)
 	for y := 0; y <= max; y++ {
-		ts := newTuples()
-
 		for _, r := range rs {
-			x1, x2, ok := r.seenInRow(y)
+			x1, x2, ok = r.seenInRow(y)
 			if ok {
 				ts.add(x1, x2)
 			}
 		}
-		output := ts.generate()
+		output = ts.generate()
+		// this assumes there's always output for the row
 		for _, t := range output {
 			if t.t2 < 0 {
 				continue
