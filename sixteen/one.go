8199371aa6f3549865b02789b0b582643ad8a524
Josh Przybyszewski <josh.prezby@gmail.com>
2022-12-19 09:27:49 -0600

    next attempt

--- sixteen/one.go
+++ sixteen/one.go
@@ -43,15 +43,25 @@ func getBestPath(
 	)
 	max := 0
 
+	all := make(map[string]struct{}, len(ns)-1)
+	for name := range ns {
+		all[name] = struct{}{}
+	}
+
 	for _, e := range es {
 		fmt.Printf("Starting at %q\n\twith %d time remaining\n",
 			e.dest.name,
 			timeRemaining-e.weight,
 		)
+
+		s := state{
+			cur:           e.dest.name,
+			ns:            ns,
+			closed:        all,
+			timeRemaining: timeRemaining - e.weight,
+		}
 		em := maximize(
-			e.dest.name,
-			ns,
-			timeRemaining-e.weight,
+			s,
 		)
 
 		if em > max {
@@ -62,77 +72,73 @@ func getBestPath(
 	return max
 }
 
-func maximize(
-	start string,
-	ns map[string]*node,
-	timeRemaining int,
-) int {
-	if timeRemaining <= 0 {
-		return 0
-	}
+type state struct {
+	cur string
 
-	all := make(map[string]struct{}, len(ns)-1)
-	for name := range ns {
-		if name == start {
-			continue
-		}
-		all[name] = struct{}{}
-	}
+	ns     map[string]*node
+	closed map[string]struct{}
 
-	return ((timeRemaining - 1) * ns[start].value) + next(
-		start,
-		ns,
-		all,
-		timeRemaining-1,
-	)
+	timeRemaining    int
+	pressureReleased int
 }
 
-func next(
-	cur string,
-	ns map[string]*node,
-	closed map[string]struct{},
-	timeRemaining int,
-) int {
-	// fmt.Printf("\tLocated at %q\n",
-	// 	cur,
-	// )
-	// fmt.Printf("\t\twith %d time remaining\n",
-	// 	timeRemaining,
-	// )
+func (s state) travel(
+	e *edge,
+) (state, bool) {
+	if s.timeRemaining <= 0 {
+		// no time
+		return s, false
+	}
 
-	if timeRemaining <= 0 {
+	if _, ok := s.closed[e.dest.name]; !ok {
+		// the destination isn't closed. Not worth going to.
+		return s, false
+	}
+
+	return state{
+		cur:           e.dest.name,
+		ns:            s.ns,
+		closed:        s.closed,
+		timeRemaining: s.timeRemaining - e.weight,
+	}, true
+}
+
+func (s state) open() (state, bool) {
+	if s.timeRemaining <= 0 {
+		// no time
+		return s, false
+	}
+
+	s2 := state{
+		cur:              s.cur,
+		ns:               s.ns,
+		closed:           openValve(s.cur, s.closed),
+		timeRemaining:    s.timeRemaining - 1,
+		pressureReleased: s.pressureReleased,
+	}
+
+	s2.pressureReleased += (s2.timeRemaining * s2.ns[s2.cur].value)
+
+	return s2, true
+}
+
+func maximize(
+	s state,
+) int {
+	os, ok := s.open()
+	if !ok {
 		return 0
 	}
 
-	max := 0
-
-	// check the destinations we could move to
-	for _, e := range ns[cur].edges {
-		if e.weight >= timeRemaining {
-			// it'll take longer to get there than any benefit we receive
+	max := os.pressureReleased
+	for _, e := range s.ns[s.cur].edges {
+		ts, ok := os.travel(e)
+		if !ok {
 			continue
 		}
-		if _, ok := closed[e.dest.name]; !ok {
-			// the valve at this dest has already been opened!
-			continue
-		}
-
-		// navigate to the destination and open the valve.
-		// This will release a total pressure of the valve's
-		// value * how long it will be open.
-		// The valve will be open for the time remaining minus
-		// the time it takes to get there minus one minute to open it.
-		// == (timeRemaining - (e.weight + 1))
-		val := (timeRemaining - e.weight - 1) * e.dest.value
-
-		then := next(
-			e.dest.name,
-			ns,
-			openValve(e.dest.name, closed),
-			timeRemaining-e.weight-1,
-		)
-		if val+then > max {
-			max = val + then
+		tmax := maximize(ts)
+		if tmax > max {
+			max = tmax
 		}
 	}
 
@@ -143,9 +149,6 @@ func openValve(
 	d string,
 	closed map[string]struct{},
 ) map[string]struct{} {
-	// fmt.Printf("\tOPENING %q\n",
-	// 	d,
-	// )
 	c2 := make(map[string]struct{}, len(closed)-1)
 	for c := range closed {
 		if c == d {
