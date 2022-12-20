e075ccb9e7684b1094a9b7e95b31e47031b85e10
Joshua Przybyszewski <josh.prezby@gmail.com>
2022-12-20 05:36:59 -0600

    update part 2

--- sixteen/two.go
+++ sixteen/two.go
@@ -35,7 +35,7 @@ func getBestPathForTwoTravellers(
 		}
 	}
 
-	var best, em pathState2
+	var best, em pairPaths
 
 	for n1, d1 := range g.startingPositions {
 		for n2, d2 := range g.startingPositions {
@@ -46,11 +46,15 @@ func getBestPathForTwoTravellers(
 			// fmt.Printf("Starting from %d (spent %d time)\n", n, d)
 			em = maximize2(
 				g,
-				pathState2{
-					cur1:       node(n1),
-					cur2:       node(n2),
-					remaining1: remaining - time(d1),
-					remaining2: remaining - time(d2),
+				pairPaths{
+					one: traveller{
+						cur:       node(n1),
+						remaining: remaining - time(d1),
+					},
+					two: traveller{
+						cur:       node(n2),
+						remaining: remaining - time(d2),
+					},
 				},
 			)
 			// fmt.Printf("best path from %d is: %+v\n\n", n, em)
@@ -70,22 +74,26 @@ func getBestPathForTwoTravellers(
 	return int(best.released)
 }
 
-type pathState2 struct {
-	cur1, cur2             node
-	remaining1, remaining2 time
+type traveller struct {
+	cur       node
+	remaining time
+}
+
+type pairPaths struct {
+	one, two traveller
 
 	valves valveState
 
 	released pressure
 
-	prev *pathState2
+	prev *pairPaths
 }
 
-func (s pathState2) isOpen(n node) bool {
+func (s pairPaths) isOpen(n node) bool {
 	return s.valves.isOpen(n)
 }
 
-func (s pathState2) String(
+func (s pairPaths) String(
 	names [numNodes]string,
 ) string {
 	return `TODO`
@@ -114,58 +122,18 @@ func (s pathState2) String(
 	// 	)
 }
 
-func travel2(
-	g graph,
-	s pathState2,
-	dest node,
-) (pathState2, bool) {
-	if s.remaining1 <= 0 && s.remaining2 <= 0 {
-		// fmt.Printf("\tCannot travel. No time remaining\n")
-		// no time
-		return s, false
-	}
-
-	if s.isOpen(dest) {
-		// fmt.Printf("\tCannot travel. already open\n")
-		// the destination is already open. Not worth going to.
-		return s, false
-	}
-
-	if s.remaining1 >= s.remaining2 {
-		s2 := s
-		s2.prev = &s
-		s2.remaining1 -= time(g.getDistance(s2.cur1, dest))
-		s2.cur1 = dest
-
-		return s2, true
-	}
-
-	// fmt.Printf("\tTraveling from %2d to %2d (distance %d)\n",
-	// 	s.cur,
-	// 	dest,
-	// 	g.getDistance(s.cur, dest),
-	// )
-
-	s2 := s
-	s2.prev = &s
-	s2.remaining2 -= time(g.getDistance(s2.cur2, dest))
-	s2.cur2 = dest
-
-	return s2, true
-}
-
 func open2(
 	g graph,
-	s pathState2,
-) (pathState2, bool) {
-	if s.remaining1 <= 0 && s.remaining2 <= 0 {
+	s pairPaths,
+) (pairPaths, bool) {
+	if s.one.remaining <= 0 && s.two.remaining <= 0 {
 		// fmt.Printf("\tCannot open. No time remaining\n")
 		// no time
 		return s, false
 	}
 
-	if s.remaining1 >= s.remaining2 {
-		if s.isOpen(s.cur1) {
+	if s.one.remaining > s.two.remaining {
+		if s.isOpen(s.two.cur) {
 			// fmt.Printf("\tCannot open. already open\n")
 			// the current node is already open. Cannot open
 			return s, false
@@ -178,13 +146,13 @@ func open2(
 
 		s2 := s
 		s2.prev = &s
-		s2.valves = s2.valves.open(s2.cur1)
-		s2.remaining1 -= 1
-		s2.released += (pressure(s2.remaining1) * pressure(g.getValue(s2.cur1)))
+		s2.valves = s2.valves.open(s2.one.cur)
+		s2.one.remaining -= 1
+		s2.released += (pressure(s2.one.remaining) * pressure(g.getValue(s2.one.cur)))
 		return s2, true
 	}
 
-	if s.isOpen(s.cur2) {
+	if s.isOpen(s.two.cur) {
 		// fmt.Printf("\tCannot open. already open\n")
 		// the current node is already open. Cannot open
 		return s, false
@@ -197,36 +165,103 @@ func open2(
 
 	s2 := s
 	s2.prev = &s
-	s2.valves = s2.valves.open(s2.cur2)
-	s2.remaining2 -= 1
-	s2.released += (pressure(s2.remaining2) * pressure(g.getValue(s2.cur2)))
+	s2.valves = s2.valves.open(s2.two.cur)
+	s2.two.remaining -= 1
+	s2.released += (pressure(s2.two.remaining) * pressure(g.getValue(s2.two.cur)))
 
 	return s2, true
 }
 
 func maximize2(
 	g graph,
-	s pathState2,
-) pathState2 {
-	s, ok := open2(g, s)
-	if !ok {
+	s pairPaths,
+) pairPaths {
+	if s.one.remaining <= 0 && s.two.remaining <= 0 {
+		// fmt.Printf("\tCannot open. No time remaining\n")
+		// no time
 		return s
 	}
 
-	best := s
-	var ts pathState2
-	for i := 0; i < numNodes; i++ {
-		ts, ok = travel2(
-			g,
-			s,
-			node(i),
-		)
+	if s.one.remaining != s.two.remaining {
+		useOne := s.one.remaining > s.two.remaining
+		s, ok := open2(g, s)
 		if !ok {
+			return s
+		}
+
+		// travel and maximize
+		best := s
+		var ts pairPaths
+		for i := 0; i < numNodes; i++ {
+			if s.isOpen(node(i)) {
+				continue
+			}
+			if useOne && ts.two.cur == node(i) {
+				continue
+			} else if !useOne && ts.one.cur == node(i) {
+				continue
+			}
+
+			ts = s
+			ts.prev = &s
+			if useOne {
+				ts.one.remaining -= time(g.getDistance(ts.one.cur, node(i)))
+				ts.one.cur = node(i)
+			} else {
+				ts.two.remaining -= time(g.getDistance(ts.two.cur, node(i)))
+				ts.two.cur = node(i)
+			}
+
+			tmax := maximize2(g, ts)
+			if tmax.released > best.released {
+				best = tmax
+			}
+		}
+
+		return best
+	}
+
+	if s.isOpen(s.one.cur) {
+		s2 := s
+		s2.prev = &s
+		s2.valves = s2.valves.open(s2.one.cur)
+		s2.one.remaining -= 1
+		s2.released += (pressure(s2.one.remaining) * pressure(g.getValue(s2.one.cur)))
+		s = s2
+	}
+	if s.isOpen(s.two.cur) {
+		s2 := s
+		s2.prev = &s
+		s2.valves = s2.valves.open(s2.two.cur)
+		s2.two.remaining -= 1
+		s2.released += (pressure(s2.two.remaining) * pressure(g.getValue(s2.two.cur)))
+		s = s2
+	}
+
+	best := s
+	for i := 0; i < numNodes; i++ {
+		if s.isOpen(node(i)) || s.one.cur == node(i) || s.two.cur == node(i) {
 			continue
 		}
-		tmax := maximize2(g, ts)
-		if tmax.released > best.released {
-			best = tmax
+		for j := 0; j < numNodes; j++ {
+			if i == j {
+				continue
+			}
+			if s.isOpen(node(j)) || s.one.cur == node(j) || s.two.cur == node(j) {
+				continue
+			}
+
+			s2 := s
+			s2.prev = &s
+			s2.one.remaining -= time(g.getDistance(s2.one.cur, node(i)))
+			s2.one.cur = node(i)
+			s2.two.remaining -= time(g.getDistance(s2.two.cur, node(j)))
+			s2.two.cur = node(j)
+
+			tmax := maximize2(g, s2)
+			if tmax.released > best.released {
+				best = tmax
+			}
 		}
 	}
 
