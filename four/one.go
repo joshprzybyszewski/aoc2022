41c08bd1afa81f415f12b582573977e4086af0c7
Josh Przybyszewski <josh.prezby@gmail.com>
2022-12-20 19:24:07 -0600

    no allocs in four

--- four/one.go
+++ four/one.go
@@ -8,14 +8,14 @@ import (
 func One(
 	input string,
 ) (int, error) {
-	lines := strings.Split(input, "\n")
 
 	var s1, e1, s2, e2, i1, i2 int
 	var err error
 	total := 0
 
-	for _, line := range lines {
-		if line == `` {
+	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
+		if nli == 0 {
+			input = input[1:]
 			continue
 		}
 
@@ -23,35 +23,36 @@ func One(
 		// strings.Split takes 3000 allocs, but 112000 B
 		// Using Index takes 2 allocs, and 16387 B
 		i1 = 0
-		i2 = strings.Index(line, `-`)
-		s1, err = strconv.Atoi(line[i1:i2])
+		i2 = strings.Index(input, `-`)
+		s1, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = strings.Index(line, `,`)
-		e1, err = strconv.Atoi(line[i1:i2])
+		i2 = strings.Index(input, `,`)
+		e1, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = strings.LastIndex(line, `-`)
-		s2, err = strconv.Atoi(line[i1:i2])
+		i2 = i1 + strings.Index(input[i1:], `-`)
+		s2, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = len(line)
-		e2, err = strconv.Atoi(line[i1:i2])
+		i2 = nli
+		e2, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 
 		if (s1 >= s2 && e1 <= e2) ||
 			(s2 >= s1 && e2 <= e1) {
-			// the ranges are overlapping
+			// the ranges are completely overlapping
 			total++
 		}
+		input = input[nli+1:]
 	}
 
 	return total, nil
--- four/two.go
+++ four/two.go
@@ -8,38 +8,38 @@ import (
 func Two(
 	input string,
 ) (int, error) {
-	lines := strings.Split(input, "\n")
 
 	var s1, e1, s2, e2, i1, i2 int
 	var err error
 	total := 0
 
-	for _, line := range lines {
-		if line == `` {
+	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
+		if nli == 0 {
+			input = input[1:]
 			continue
 		}
 
 		i1 = 0
-		i2 = strings.Index(line, `-`)
-		s1, err = strconv.Atoi(line[i1:i2])
+		i2 = strings.Index(input, `-`)
+		s1, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = strings.Index(line, `,`)
-		e1, err = strconv.Atoi(line[i1:i2])
+		i2 = strings.Index(input, `,`)
+		e1, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = strings.LastIndex(line, `-`)
-		s2, err = strconv.Atoi(line[i1:i2])
+		i2 = i1 + strings.Index(input[i1:], `-`)
+		s2, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
 		i1 = i2 + 1
-		i2 = len(line)
-		e2, err = strconv.Atoi(line[i1:i2])
+		i2 = nli
+		e2, err = strconv.Atoi(input[i1:i2])
 		if err != nil {
 			return 0, err
 		}
@@ -48,9 +48,10 @@ func Two(
 			(e1 >= s2 && e1 <= e2) ||
 			(s2 >= s1 && s2 <= e1) ||
 			(e2 >= s1 && e2 <= e1) {
-			// the ranges are overlapping
+			// the ranges are partially overlapping
 			total++
 		}
+		input = input[nli+1:]
 	}
 
 	return total, nil
