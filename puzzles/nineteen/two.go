92664f21b572177893a2096a766292fff8c6e992
Josh Przybyszewski <josh.prezby@gmail.com>
2023-01-05 10:07:28 -0600

    solve part 2

--- puzzles/nineteen/two.go
+++ puzzles/nineteen/two.go
@@ -1,8 +1,20 @@
 package nineteen
 
+const (
+	part2Minutes = 32
+)
+
 func Two(
 	input string,
 ) (int, error) {
+	all, err := getBlueprints(input)
+	if err != nil {
+		return 0, err
+	}
 
-	return 0, nil
+	a := harvest(&all[0], part2Minutes)
+	b := harvest(&all[1], part2Minutes)
+	c := harvest(&all[2], part2Minutes)
+
+	return a * b * c, nil
 }
