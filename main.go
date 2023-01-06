d3b9dda8011cde92d80ff68c6cb7c497af17b428
Josh Przybyszewski <josh.prezby@gmail.com>
2023-01-06 10:09:09 -0600

    now the main runs all of them in 2.140s

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"runtime"
+	"sync"
 
 	"github.com/joshprzybyszewski/aoc2022/util"
 )
@@ -27,11 +29,9 @@ func main() {
 		return
 	}
 
-	for day := 1; day <= 25; day++ {
-		err := runDay(day)
-		if err != nil {
-			panic(err)
-		}
+	err := runAll()
+	if err != nil {
+		panic(err)
 	}
 }
 
@@ -47,19 +47,6 @@ func runDay(
 	part1, part2 = util.Instrument(part1, part2)
 	part1, part2 = util.Submit(day, part1, part2)
 
-	runParts(
-		day,
-		input,
-		part1, part2,
-	)
-	return nil
-}
-
-func runParts(
-	day int,
-	input string,
-	part1, part2 func(string) (string, error),
-) {
 	fmt.Printf("=====================================\n")
 	fmt.Printf("Day %d\n", day)
 	fmt.Printf("-------------------------------------\n")
@@ -67,7 +54,7 @@ func runParts(
 	answer, err := part1(input)
 	if err != nil {
 		fmt.Printf("Part 1 error: %q\n", err)
-		panic(err)
+		return err
 	}
 	fmt.Printf("Part 1 Answer: %q\n", answer)
 
@@ -76,7 +63,70 @@ func runParts(
 	answer, err = part2(input)
 	if err != nil {
 		fmt.Printf("Part 2 error: %q\n", err)
-		panic(err)
+		return err
 	}
 	fmt.Printf("Part 2 Answer: %q\n\n\n", answer)
+
+	return nil
+}
+
+func runAll() error {
+	var inputs [25]string
+	var solvers [25][2]func(string) (string, error)
+	var answers [25][2]string
+
+	for day := 1; day <= 25; day++ {
+		input, err := util.Input(day)
+		if err != nil {
+			return err
+		}
+		inputs[day-1] = input
+
+		part1, part2 := util.Solvers(day)
+		solvers[day-1][0] = part1
+		solvers[day-1][1] = part2
+	}
+
+	getAnswer := func(day, part int) {
+		if answers[day-1][part] != `` {
+			return
+		}
+		answers[day-1][part], _ = solvers[day-1][part](inputs[day-1])
+	}
+
+	// these solvers already use concurrency: don't tromp on them.
+	getAnswer(11, 1)
+	getAnswer(19, 0)
+	getAnswer(19, 1)
+	getAnswer(16, 0)
+	getAnswer(16, 1)
+	getAnswer(23, 0)
+	getAnswer(23, 1)
+
+	var wg sync.WaitGroup
+	work := make(chan int, 50)
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func() {
+			for w := range work {
+				getAnswer(w/2+1, w%2)
+				wg.Done()
+			}
+		}()
+	}
+
+	for i := 0; i < 49; i++ {
+		wg.Add(1)
+		work <- i
+	}
+
+	wg.Wait()
+
+	for day := 1; day <= 25; day++ {
+		fmt.Printf("=====================\n")
+		fmt.Printf("Day %2d\n", day)
+		fmt.Printf("\tPart 1: %q\n", answers[day-1][0])
+		fmt.Printf("\tPart 2: %q\n\n", answers[day-1][1])
+	}
+
+	return nil
 }
