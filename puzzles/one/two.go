4a64d8214199e68d7a2931ceda75a9f87d7de027
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-02 13:30:17 +0000

    start solving 2023 day 2

--- puzzles/one/two.go
+++ puzzles/one/two.go
@@ -1,7 +1,7 @@
 package one
 
 import (
-	"strconv"
+	"bytes"
 	"strings"
 )
 
--- puzzles/two/one.go
+++ puzzles/two/one.go
@@ -8,89 +8,43 @@ import (
 func One(
 	input string,
 ) (int, error) {
-	var s int
-	var err error
-
-	total := 0
-	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		if nli == 0 {
-			input = input[1:]
-			continue
-		}
-		s, err = score(input[0:nli])
-		if err != nil {
-			return 0, err
-		}
-		total += s
-		input = input[nli+1:]
+	max := handful{
+		red: 12,
+		green: 13,
+		blue: 14,
 	}
 
-	return total, nil
+	lines := strings.Split(input, "\n")
+
+	sum := 0
+	for i, line := range lines {
+
+	handfuls := strings.Split(line, ";")
+	for _, handfulStr := range handfuls {
+		handful :=interpretSeen(handfulStr)
+		if !isPossible(handful, max) {
+			sum += (i+1)
+		}
+	}
+
+		
+	}
+
+	return sum, nil
 }
 
-func score(
-	line string,
-) (int, error) {
-	ss, err := shapeScore(line[2])
-	if err != nil {
-		return 0, err
-	}
+func interpretSeen(handfulString string) handful {
 
-	ws, err := winScore(line[0], line[2])
-	if err != nil {
-		return 0, err
-	}
-
-	return ss + ws, nil
 }
 
-func shapeScore(
-	encChar byte,
-) (int, error) {
-	switch encChar {
-	case 'X':
-		return 1, nil
-	case 'Y':
-		return 2, nil
-	case 'Z':
-		return 3, nil
-	}
-
-	return 0, fmt.Errorf(`unsupported char: %q`, encChar)
+type handful struct{
+	red int
+	blue int
+	green int
 }
 
-func winScore(
-	encChar1, encChar2 byte,
-) (int, error) { // nolint:gocyclo yes i know
-	switch encChar2 {
-	case 'X': // rock
-		switch encChar1 {
-		case 'A': // rock
-			return 3, nil
-		case 'B': // paper
-			return 0, nil
-		case 'C': // scissors
-			return 6, nil
-		}
-	case 'Y': // paper
-		switch encChar1 {
-		case 'A': // rock
-			return 6, nil
-		case 'B': // paper
-			return 3, nil
-		case 'C': // scissors
-			return 0, nil
-		}
-	case 'Z': // scissors
-		switch encChar1 {
-		case 'A': // rock
-			return 0, nil
-		case 'B': // paper
-			return 6, nil
-		case 'C': // scissors
-			return 3, nil
-		}
-	}
+func isPossible(
+	seen, max handful,
+) bool {
 
-	return 0, fmt.Errorf(`unsupported chars: %q %q`, encChar1, encChar2)
 }
