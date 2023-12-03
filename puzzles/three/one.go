544cca4ca533dac0e9ecb748b7a8dc7d53a2b6fe
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-03 13:02:03 +0000

    solve day 3

--- puzzles/three/one.go
+++ puzzles/three/one.go
@@ -1,51 +1,116 @@
 package three
 
 import (
-	"fmt"
 	"strings"
 )
 
+type coord struct {
+	row int
+	col int
+}
+
+type symbols map[coord]byte
+
+func newSymbols() symbols {
+	return make(symbols)
+}
+
+func (s symbols) add(row, col int, c byte) {
+	s[coord{
+		row: row,
+		col: col,
+	}] = c
+}
+
+func (s symbols) isNextToSymbol(row, col int) bool {
+	// check left, then below, then right, then above
+	tmp := coord{
+		row: row,
+		col: col - 1,
+	}
+	_, ok := s[tmp]
+	if ok {
+		return true
+	}
+	tmp.row--
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.col++
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.col++
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.row++
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.row++
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.col--
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	tmp.col--
+	_, ok = s[tmp]
+	if ok {
+		return true
+	}
+	return false
+}
+
 func One(
-	input string,
+	fullInput string,
 ) (int, error) {
-	var c byte
-	var err error
-	total := 0
+	input := fullInput
+
+	var row, col int
+	s := newSymbols()
+
 	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		if nli == 0 {
-			input = input[1:]
-			continue
+		for col = 0; col < nli; col++ {
+			if input[col] == '.' || (input[col] >= '0' && input[col] <= '9') {
+				continue
+			}
+			s.add(row, col, input[col])
 		}
-		c, err = common(input[0:nli])
-		if err != nil {
-			return 0, err
-		}
-		total += priority(c)
+		row++
 		input = input[nli+1:]
 	}
 
-	return total, nil
-}
-
-func common(
-	line string,
-) (byte, error) {
-	half := len(line) / 2
-
-	for i := 0; i < half; i++ {
-		if strings.ContainsRune(line[half:], rune(line[i])) {
-			return line[i], nil
+	total := 0
+	curNum := 0
+	shouldAdd := false
+	row = 0
+	input = fullInput
+	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
+		for col = 0; col < nli; col++ {
+			if input[col] < '0' || input[col] > '9' {
+				if shouldAdd {
+					total += curNum
+					shouldAdd = false
+				}
+				curNum = 0
+				continue
+			}
+			curNum *= 10
+			curNum += int(input[col] - '0')
+			shouldAdd = shouldAdd || s.isNextToSymbol(row, col)
 		}
+		input = input[nli+1:]
+		row++
 	}
 
-	return 0, fmt.Errorf("couldn't find common element: %q %q", line[:half], line[half:])
-}
-
-func priority(
-	c byte,
-) int {
-	if c >= 'a' && c <= 'z' {
-		return int(c-'a') + 1
-	}
-	return int(c-'A') + 27
+	return total, nil
 }
--- puzzles/three/two.go
+++ puzzles/three/two.go
@@ -1,40 +1,20 @@
 package three
 
-import (
-	"fmt"
-	"strings"
-)
-
 func Two(
 	input string,
 ) (int, error) {
-	lines := strings.Split(input, "\n")
+	// lines := strings.Split(input, "\n")
 
-	var c byte
-	var err error
-	total := 0
-	for i := 0; i+3 <= len(lines); i += 3 {
-		c, err = commonLetter(lines[i : i+3])
-		if err != nil {
-			return 0, err
-		}
-		total += priority(c)
-	}
+	// var c byte
+	// var err error
+	// total := 0
+	// for i := 0; i+3 <= len(lines); i += 3 {
+	// 	c, err = commonLetter(lines[i : i+3])
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	total += priority(c)
+	// }
 
-	return total, nil
-}
-
-func commonLetter(
-	lines []string,
-) (byte, error) {
-	for i := range lines[0] {
-		if !strings.ContainsRune(lines[1], rune(lines[0][i])) {
-			continue
-		}
-		if !strings.ContainsRune(lines[2], rune(lines[0][i])) {
-			continue
-		}
-		return lines[0][i], nil
-	}
-	return 0, fmt.Errorf("couldn't find common element: %q %q %q", lines[0], lines[1], lines[2])
+	return 0, nil
 }
