db36581407c1e4c116679f5b1404fe3032f1d875
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-03 00:04:48 +0000

    solve day 2

--- puzzles/two/two.go
+++ puzzles/two/two.go
@@ -1,99 +1,41 @@
 package two
 
 import (
-	"fmt"
 	"strings"
 )
 
 func Two(
 	input string,
 ) (int, error) {
-	var s int
-	var err error
 
-	total := 0
-	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		if nli == 0 {
-			input = input[1:]
+	lines := strings.Split(input, "\n")
+
+	sum := 0
+	for _, line := range lines {
+		handfuls := strings.Split(
+			line[strings.Index(line, `:`)+1:],
+			";",
+		)
+		if len(handfuls) == 0 {
 			continue
 		}
-		s, err = score2(input[0:nli])
-		if err != nil {
-			return 0, err
+		min := interpretSeen(handfuls[0])
+		for _, handfulStr := range handfuls {
+			handful := interpretSeen(handfulStr)
+			if handful.red > min.red {
+				min.red = handful.red
+			}
+			if handful.green > min.green {
+				min.green = handful.green
+			}
+			if handful.blue > min.blue {
+				min.blue = handful.blue
+			}
 		}
-		total += s
-		input = input[nli+1:]
+
+		sum += (min.red * min.blue * min.green)
+
 	}
 
-	return total, nil
-}
-
-func score2(
-	line string,
-) (int, error) {
-	ss, err := shapeScore2(line[0], line[2])
-	if err != nil {
-		return 0, err
-	}
-
-	ws, err := winScore2(line[2])
-	if err != nil {
-		return 0, err
-	}
-
-	return ss + ws, nil
-}
-
-func shapeScore2(
-	encChar,
-	encOutcome byte,
-) (int, error) {
-
-	opp := 0
-	switch encChar {
-	case 'A': // rock
-		opp = 1
-	case 'B': // paper
-		opp = 2
-	case 'C': // scissors
-		opp = 3
-	default:
-		return 0, fmt.Errorf(`unsupported char: %q`, encChar)
-	}
-
-	switch encOutcome {
-	case 'X': // lose
-		// i'm sure i could modulo this
-		mine := opp - 1
-		if mine == 0 {
-			return 3, nil
-		}
-		return mine, nil
-	case 'Y': // draw
-		return opp, nil
-	case 'Z': // win
-		// i'm sure i could modulo this
-		mine := opp + 1
-		if mine == 4 {
-			return 1, nil
-		}
-		return mine, nil
-	}
-
-	return 0, fmt.Errorf(`unsupported char: %q %q`, encChar, encOutcome)
-}
-
-func winScore2(
-	encOutcome byte,
-) (int, error) {
-	switch encOutcome {
-	case 'X': // lose
-		return 0, nil
-	case 'Y': // draw
-		return 3, nil
-	case 'Z': // win
-		return 6, nil
-	}
-
-	return 0, fmt.Errorf(`unsupported chars: %q`, encOutcome)
+	return sum, nil
 }
