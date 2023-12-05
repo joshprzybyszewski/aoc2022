c00cd82c0503b578409aabc1ef6fab3f7bc63747
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-05 02:47:51 +0000

    speed it up a bit

--- puzzles/four/two.go
+++ puzzles/four/two.go
@@ -9,10 +9,13 @@ func Two(
 	input string,
 ) (int, error) {
 
-	// skip the zero index
-	allCards := make([]card, 1, 200)
+	// allCards := make([]card, 0, 200)
+	numCopies := make([]int, 200)
+	total := 0
 
-	var ci, pi int
+	tmp, cardCopies, j := 0, 0, 0
+
+	var i, ci, pi int
 
 	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
 		if nli == 0 {
@@ -23,9 +26,6 @@ func Two(
 		ci = strings.Index(input, ":") + 1
 		pi = strings.Index(input, "|")
 
-		// TODO the numbers are not necessarily delimited by a
-		// space characer because they're pretty-printed to line
-		// up with columns above and below
 		winners := strings.Split(
 			strings.TrimSpace(input[ci:pi]),
 			" ",
@@ -37,45 +37,38 @@ func Two(
 
 		c := newCard(max(len(winners), len(shown)))
 		for _, w := range winners {
+			if w == `` {
+				continue
+			}
 			v, err := strconv.Atoi(w)
 			if err != nil {
-				// TODO reconsider
-				continue
+				panic(`ahh`)
 			}
 			c = c.addWinner(v)
 		}
 
 		for _, s := range shown {
+			if s == `` {
+				continue
+			}
 			v, err := strconv.Atoi(s)
 			if err != nil {
-				// TODO reconsider
-				continue
+				panic(err)
 			}
 			c = c.addShown(v)
 		}
-		allCards = append(allCards, c)
 
-		input = input[nli+1:]
-	}
-
-	numCopies := make([]int, len(allCards))
-
-	tmp, cardCopies := 0, 0
-
-	for i := 1; i < len(allCards); i++ {
 		// one original copy.
-		numCopies[i]++
+		cardCopies = numCopies[i] + 1
+		total += cardCopies
 
-		cardCopies = numCopies[i]
-		tmp = i + allCards[i].numMatching() + 1
-		for j := i + 1; j < tmp; j++ {
+		tmp = i + c.numMatching() + 1
+		for j = i + 1; j < tmp; j++ {
 			numCopies[j] += cardCopies
 		}
-	}
 
-	total := 0
-	for _, n := range numCopies {
-		total += n
+		input = input[nli+1:]
+		i++
 	}
 
 	return total, nil
