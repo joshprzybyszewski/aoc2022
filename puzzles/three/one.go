80e3620cd429b2715223ed4345c9ef984acdd2f6
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-06 17:28:28 +0000

    make day 3 part 1 faster

--- puzzles/three/one.go
+++ puzzles/three/one.go
@@ -9,64 +9,54 @@ type coord struct {
 	col int
 }
 
-type symbols map[coord]byte
+type symbols struct {
+	symbols [size][size]bool
+}
 
 func newSymbols() symbols {
-	return make(symbols)
+	return symbols{}
 }
 
-func (s symbols) add(row, col int, c byte) {
-	s[coord{
-		row: row,
-		col: col,
-	}] = c
+func (s *symbols) addSymbol(row, col int, c byte) {
+	if c == '.' || (c >= '0' && c <= '9') {
+		return
+	}
+
+	s.symbols[row][col] = true
 }
 
-func (s symbols) isNextToSymbol(row, col int) bool {
-	// check left, then below, then right, then above
-	tmp := coord{
-		row: row,
-		col: col - 1,
+func (s *symbols) isNextToSymbol(row, col int, numDigits int) bool {
+	minCol := col - 1 - numDigits
+	if minCol < 0 {
+		minCol = 0
 	}
-	_, ok := s[tmp]
-	if ok {
+
+	if col == size {
+		col--
+	}
+
+	if s.symbols[row][col] || s.symbols[row][minCol] {
 		return true
 	}
-	tmp.row--
-	_, ok = s[tmp]
-	if ok {
-		return true
+
+	var tmpCol int
+
+	if row > 0 {
+		for tmpCol = col; tmpCol >= minCol; tmpCol-- {
+			if s.symbols[row-1][tmpCol] {
+				return true
+			}
+		}
 	}
-	tmp.col++
-	_, ok = s[tmp]
-	if ok {
-		return true
+
+	if row+1 < size {
+		for tmpCol = col; tmpCol >= minCol; tmpCol-- {
+			if s.symbols[row+1][tmpCol] {
+				return true
+			}
+		}
 	}
-	tmp.col++
-	_, ok = s[tmp]
-	if ok {
-		return true
-	}
-	tmp.row++
-	_, ok = s[tmp]
-	if ok {
-		return true
-	}
-	tmp.row++
-	_, ok = s[tmp]
-	if ok {
-		return true
-	}
-	tmp.col--
-	_, ok = s[tmp]
-	if ok {
-		return true
-	}
-	tmp.col--
-	_, ok = s[tmp]
-	if ok {
-		return true
-	}
+
 	return false
 }
 
@@ -80,33 +70,29 @@ func One(
 
 	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
 		for col = 0; col < nli; col++ {
-			if input[col] == '.' || (input[col] >= '0' && input[col] <= '9') {
-				continue
-			}
-			s.add(row, col, input[col])
+			s.addSymbol(row, col, input[col])
 		}
 		row++
 		input = input[nli+1:]
 	}
 
 	total := 0
-	curNum := 0
-	shouldAdd := false
+	curNum, numDigits := 0, 0
 	row = 0
 	input = fullInput
 	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		for col = 0; col < nli; col++ {
+		for col = 0; col <= nli; col++ {
 			if input[col] < '0' || input[col] > '9' {
-				if shouldAdd {
+				if curNum > 0 && s.isNextToSymbol(row, col, numDigits) {
 					total += curNum
-					shouldAdd = false
 				}
 				curNum = 0
+				numDigits = 0
 				continue
 			}
+			numDigits++
 			curNum *= 10
 			curNum += int(input[col] - '0')
-			shouldAdd = shouldAdd || s.isNextToSymbol(row, col)
 		}
 		input = input[nli+1:]
 		row++
--- results.md
+++ results.md
@@ -19,15 +19,15 @@ cpu: AMD EPYC 7763 64-Core Processor
 
 |Puzzle|Duration|Bytes allocated to Heap|# of Heap allocations|
 |-|-:|-:|-:|
-|Day 1 Part One|60.59 µs|22832|134|
-|Day 1 Part Two|1.88 ms|22832|134|
-|Day 2 Part One|102.84 µs|48752|1259|
-|Day 2 Part Two|183.28 µs|75536|2026|
-|Day 3 Part One|625.02 µs|45624|42|
-|Day 3 Part Two|104.72 µs|0|0|
-|Day 4 Part One|83.12 µs|0|0|
-|Day 4 Part Two|90.19 µs|0|0|
-|Day 5 Part One|46.09 µs|18528|71|
-|Day 5 Part Two|84.41 µs|18528|71|
-|Day 6 Part One|79.11 ns|0|0|
-|Day 6 Part Two|65.69 ns|0|0|
+|Day 1 Part One|59.92 µs|22832|134|
+|Day 1 Part Two|1.09 ms|22832|134|
+|Day 2 Part One|104.76 µs|48752|1259|
+|Day 2 Part Two|184.01 µs|75536|2026|
+|Day 3 Part One|122.45 µs|0|0|
+|Day 3 Part Two|121.00 µs|0|0|
+|Day 4 Part One|76.52 µs|0|0|
+|Day 4 Part Two|101.11 µs|0|0|
+|Day 5 Part One|43.83 µs|18528|71|
+|Day 5 Part Two|87.61 µs|18528|71|
+|Day 6 Part One|81.66 ns|0|0|
+|Day 6 Part Two|66.74 ns|0|0|
