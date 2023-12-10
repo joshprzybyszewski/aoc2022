1cbb30303d4efd3e2b09456bc0951356e94c4835
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-10 13:34:35 +0000

    start 10

--- puzzles/ten/one.go
+++ puzzles/ten/one.go
@@ -1,51 +1,91 @@
 package ten
 
 import (
-	"strconv"
 	"strings"
 )
+
+type coord struct {
+	row int
+	col int
+}
+
+const (
+	north pipe = 1 << 0
+	east  pipe = 1 << 1
+	south pipe = 1 << 2
+	west  pipe = 1 << 3
+
+	ne pipe = north | east
+	se      = south | east
+	sw      = south | west
+	nw      = north | west
+	ew      = east | west
+	ns      = north | south
+
+	start pipe = north | east | south | west
+)
+
+type pipe uint8
+
+func newPipe(b byte) pipe {
+	switch b {
+	case '-': // 2233
+		return ew
+	case '7': // 2027
+		return sw
+	case '|': // 2003
+		return ns
+	case 'F': // 1967
+		return ew
+	case 'L': // 1946
+		return ne
+	case 'J': // 1917
+		return nw
+	case '.':
+		return 0
+	case 'S':
+		return start
+	}
+	panic(`surprise`)
+}
+
+type pipeMap struct {
+	rows [140][140]pipe
+
+	start coord
+}
+
+func createPipeMap(
+	input string,
+) pipeMap {
+	pm := pipeMap{}
+
+	ri, ci := 0, 0
+	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
+		for ci = 0; ci < nli; ci++ {
+			pm.rows[ri][ci] = newPipe(input[ci])
+			if pm.rows[ri][ci] == start {
+				pm.start = coord{
+					row: ri,
+					col: ci,
+				}
+			}
+		}
+
+		ri++
+		input = input[nli+1:]
+	}
+
+	return pm
+}
+
+func (pm *pipeMap) stepsToFarthest() int {
+	ends := [2]coord{}
+}
 
 func One(
 	input string,
 ) (int, error) {
-	ci := -19 // the clock index (init to -19 = 20-1 to make modulo easier)
-	x := 1    // the value of the register
-	var a int // the cmd's value
-	var err error
 
-	sum := 0
-	cycle := func() {
-		if ci%40 == 0 {
-			sum += (x * (ci + 20))
-		}
-		ci++
-	}
-
-	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		if ci > 201 {
-			break
-		}
-		if nli == 0 {
-			input = input[1:]
-			continue
-		}
-		if input[:nli] == `noop` {
-			cycle()
-			input = input[nli+1:]
-			continue
-		}
-
-		// the instruction is "addx A"
-		a, err = strconv.Atoi(input[5:nli])
-		if err != nil {
-			return 0, err
-		}
-
-		cycle()
-		cycle()
-		x += a
-		input = input[nli+1:]
-	}
-
-	return sum, nil
+	return 0, nil
 }
--- puzzles/ten/two.go
+++ puzzles/ten/two.go
@@ -1,62 +1,7 @@
 package ten
 
-import (
-	"strconv"
-	"strings"
-)
-
 func Two(
 	input string,
-) (string, error) {
-	var crt [6][40]byte
-
-	r := 0
-	c := 0
-	x := 1    // the value of the register
-	var a int // the cmd's value
-	var err error
-
-	cycle := func() {
-		if x-1 <= c && c <= x+1 {
-			crt[r][c] = '#'
-		} else {
-			crt[r][c] = '.'
-		}
-		c++
-		if c == 40 {
-			r++
-			c = 0
-		}
-	}
-
-	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		if nli == 0 {
-			input = input[1:]
-			continue
-		}
-		if input[:nli] == `noop` {
-			cycle()
-			input = input[nli+1:]
-			continue
-		}
-
-		// the instruction is "addx A"
-		a, err = strconv.Atoi(input[5:nli])
-		if err != nil {
-			return ``, err
-		}
-
-		cycle()
-		cycle()
-		x += a
-		input = input[nli+1:]
-	}
-
-	/* Manually inspect the printed characters, then set the returned string
-	for i := range crt {
-		fmt.Printf("%s\n", crt[i])
-	}
-	*/
-
-	return `BJFRHRFU`, nil
+) (int, error) {
+	return 0, nil
 }
