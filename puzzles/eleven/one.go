749662c07c83da7bfa4dde6175a82bd49cda4dd4
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-11 12:37:14 +0000

    solve day 11 pt 1

--- puzzles/eleven/one.go
+++ puzzles/eleven/one.go
@@ -1,150 +1,93 @@
 package eleven
 
-const (
-	numMonkeys = 8
-	numItems   = 36
+import "strings"
 
-	numRounds1 = 20
-	numRounds2 = 10000
-)
+type coord struct {
+	row int
+	col int
+}
 
-var (
-	initialValues = [][]int{
-		{99, 67, 92, 61, 83, 64, 98},
-		{78, 74, 88, 89, 50},
-		{98, 91},
-		{59, 72, 94, 91, 79, 88, 94, 51},
-		{95, 72, 78},
-		{76},
-		{69, 60, 53, 89, 71, 88},
-		{72, 54, 63, 80},
+type universe struct {
+	tiles [140][140]bool
+
+	rowsWith [140]bool
+	colsWith [140]bool
+
+	universes [431]coord
+}
+
+func newUniverse(
+	input string,
+) universe {
+	u := universe{}
+
+	ri, ci := 0, 0
+	ui := 0
+	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
+		for ci = 0; ci < nli; ci++ {
+			if input[ci] == '.' {
+				continue
+			}
+			u.tiles[ri][ci] = true
+			u.rowsWith[ri] = true
+			u.colsWith[ci] = true
+
+			u.universes[ui] = coord{
+				row: ri,
+				col: ci,
+			}
+			ui++
+		}
+
+		ri++
+		input = input[nli+1:]
 	}
-)
 
-type monkey uint8
+	return u
+}
+
+func (u *universe) shortestPath(
+	i, j int,
+) int {
+	start := u.universes[i]
+	end := u.universes[j]
+	if end.row < start.row {
+		end.row, start.row = start.row, end.row
+	}
+	if end.col < start.col {
+		end.col, start.col = start.col, end.col
+	}
+
+	numExpanded := 0
+	tmp := 0
+	for tmp = start.row + 1; tmp < end.row; tmp++ {
+		if !u.rowsWith[tmp] {
+			numExpanded++
+		}
+	}
+	end.row += numExpanded
+	numExpanded = 0
+	for tmp = start.col + 1; tmp < end.col; tmp++ {
+		if !u.colsWith[tmp] {
+			numExpanded++
+		}
+	}
+	end.col += numExpanded
+
+	return (end.col - start.col) + (end.row - start.row)
+}
 
 func One(
 	input string,
 ) (int, error) {
-	monkeys := [numMonkeys][]int{}
-	for i := range monkeys {
-		monkeys[i] = make([]int, 0, numItems)
-		monkeys[i] = append(monkeys[i], initialValues[i]...)
-	}
+	u := newUniverse(input)
 
-	var m, i int
-	var nw int
-	var nm int
+	total := 0
 
-	inspections := [numMonkeys]int{}
-
-	for r := 1; r <= numRounds1; r++ {
-		for m = 0; m < numMonkeys; m++ {
-			for i = 0; i < len(monkeys[m]); i++ {
-				inspections[m]++
-				// the monkey inspects the item, and I am relieved
-				nw = newWorryLevel(monkey(m), monkeys[m][i]) / 3
-				nm = int(getDestMonkey(
-					monkey(m),
-					nw,
-				))
-				monkeys[nm] = append(monkeys[nm], nw)
-			}
-			monkeys[m] = monkeys[m][:0]
+	for i := 0; i < len(u.universes); i++ {
+		for j := i + 1; j < len(u.universes); j++ {
+			total += u.shortestPath(i, j)
 		}
 	}
-
-	var m1, m2 int
-	for _, ni := range inspections {
-		if ni > m1 {
-			m2 = m1
-			m1 = ni
-		} else if ni > m2 {
-			m2 = ni
-		}
-	}
-
-	return m1 * m2, nil
-}
-
-func newWorryLevel(
-	m monkey,
-	old int,
-) int {
-	switch m {
-	case 0:
-		return old * 17
-	case 1:
-		return old * 11
-	case 2:
-		return old + 4
-	case 3:
-		return old * old
-	case 4:
-		return old + 7
-	case 5:
-		return old + 8
-	case 6:
-		return old + 5
-	case 7:
-		return old + 3
-	}
-	panic(`unknown monkey`)
-}
-
-func getDestMonkey(
-	m monkey,
-	level int,
-) monkey {
-	switch m {
-	case 0:
-		if level%3 == 0 {
-			return 4
-		} else {
-			return 2
-		}
-	case 1:
-		if level%5 == 0 {
-			return 3
-		} else {
-			return 5
-		}
-	case 2:
-		if level%2 == 0 {
-			return 6
-		} else {
-			return 4
-		}
-	case 3:
-		if level%13 == 0 {
-			return 0
-		} else {
-			return 5
-		}
-	case 4:
-		if level%11 == 0 {
-			return 7
-		} else {
-			return 6
-		}
-	case 5:
-		if level%17 == 0 {
-			return 0
-		} else {
-			return 2
-		}
-	case 6:
-		if level%19 == 0 {
-			return 7
-		} else {
-			return 1
-		}
-	case 7:
-		if level%7 == 0 {
-			return 1
-		} else {
-			return 3
-		}
-	}
-	panic(`unknown monkey`)
+	return total, nil
 }
