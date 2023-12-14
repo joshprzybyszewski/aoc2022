1db40297524e4619bbac9fd23549d7efa42cb8ab
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-14 12:34:23 +0000

    getting the right answer on the first attempt feels so good

--- puzzles/fourteen/one.go
+++ puzzles/fourteen/one.go
@@ -1,163 +1,103 @@
 package fourteen
 
 import (
-	"fmt"
 	"strings"
 )
 
-func One(
-	input string,
-) (int, error) {
-	g, err := getGrid(input)
-	if err != nil {
-		return 0, err
-	}
-
-	units := 0
-	for g.addSand(0, 500) {
-		units++
-	}
-
-	return units, nil
-}
-
-type coord struct {
-	x, y int
-}
-
-type material uint8
-
 const (
-	air  material = 0
-	rock material = 1
-	sand material = 2
+	size = 100
 )
 
-type grid struct {
+type tile uint8
 
-	// part 2:
-	// min: {x:342 y:0}
-	// max: {x:658 y:159}
-	// [row][col]
-	mats [159][660]material
+const (
+	empty tile = 0
+	rock  tile = 1
+	block tile = 2
+)
 
-	maxRock int
-	floor   int
-}
-
-func newGrid() grid {
-	return grid{
-		maxRock: 0,
-		floor:   -1,
+func (t tile) toByte() byte {
+	switch t {
+	case empty:
+		return '.'
+	case rock:
+		return 'O'
+	case block:
+		return '#'
 	}
+	return '?'
 }
 
-func (g *grid) String() string {
-	tl, br := g.window()
+type platform struct {
+	tiles [size][size]tile
+}
+
+func newPlatform(input string) platform {
+	ri, ci := 0, 0
+	p := platform{}
+	for len(input) > 0 {
+		switch input[0] {
+		case '.':
+		case 'O':
+			p.tiles[ri][ci] = rock
+		case '#':
+			p.tiles[ri][ci] = block
+		case '\n':
+			ri++
+			ci = -1
+		}
+		ci++
+		input = input[1:]
+	}
+
+	return p
+}
+
+func (p platform) String() string {
 	var sb strings.Builder
-
-	sb.WriteString(fmt.Sprintf("%3d", tl.x))
-	sb.WriteString(`--v`)
-	for c := tl.x + 1; c < br.x; c++ {
-		sb.WriteByte(' ')
-	}
-	sb.WriteString(`v--`)
-	sb.WriteString(fmt.Sprintf("%3d", br.x))
-	sb.WriteByte('\n')
-
-	for r := tl.y; r <= br.y; r++ {
-		sb.WriteString(fmt.Sprintf("%3d", r))
-		sb.WriteByte(' ')
-		sb.WriteByte(':')
-		for c := tl.x; c <= br.x; c++ {
-			switch g.check(c, r) {
-			case air:
-				sb.WriteByte('.')
-			case rock:
-				sb.WriteByte('#')
-			case sand:
-				sb.WriteByte('o')
-			}
+	for ri := 0; ri < size; ri++ {
+		for ci := 0; ci < size; ci++ {
+			sb.WriteByte(p.tiles[ri][ci].toByte())
 		}
 		sb.WriteByte('\n')
 	}
 	return sb.String()
 }
 
-func (g *grid) window() (coord, coord) {
-	var br coord
-	tl := coord{
-		x: -1,
-		y: -1,
-	}
-	br.y = g.floor
-	for r := range g.mats {
-		for c := range g.mats[r] {
-			switch g.check(c, r) {
-			case rock, sand:
-				if tl.x == -1 || tl.x > c {
-					tl.x = c
-				}
-				if tl.y == -1 || tl.y > r {
-					tl.y = r
-				}
-				if br.x < c {
-					br.x = c
-				}
-				if br.y < r {
-					br.y = r
+func (p *platform) rollNorth() int {
+
+	totalLoad := 0
+
+	nextEmptySpotForCol := [size]int{}
+
+	for ri := 0; ri < size; ri++ {
+		for ci := 0; ci < size; ci++ {
+			switch p.tiles[ri][ci] {
+			case block:
+				nextEmptySpotForCol[ci] = ri + 1
+			case rock:
+				if nextEmptySpotForCol[ci] != ri {
+					p.tiles[nextEmptySpotForCol[ci]][ci] = rock
+					p.tiles[ri][ci] = empty
+
+					totalLoad += size - nextEmptySpotForCol[ci]
+
+					nextEmptySpotForCol[ci] += 1
+				} else {
+					nextEmptySpotForCol[ci] = ri + 1
+					totalLoad += size - ri
 				}
 			}
 		}
 	}
 
-	return tl, br
+	return totalLoad
 }
 
-func (g *grid) addRock(y, x int) {
-	if y > g.maxRock {
-		g.maxRock = y
-	}
+func One(
+	input string,
+) (int, error) {
+	p := newPlatform(input)
 
-	g.mats[y][x] = rock
-}
-
-func (g *grid) addFloor() {
-	g.floor = g.maxRock + 2
-}
-
-func (g *grid) addSand(y, x int) bool {
-	y1 := y + 1
-	if g.floor < 0 {
-		if y > g.maxRock {
-			return false
-		}
-	} else if y1 == g.floor {
-		// it has come to rest
-		g.mats[y][x] = sand
-		return true
-	}
-
-	if g.check(y1, x) == air {
-		// check from below
-		return g.addSand(y1, x)
-	}
-
-	if g.check(y1, x-1) == air {
-		// check down to the left
-		return g.addSand(y1, x-1)
-	}
-
-	if g.check(y1, x+1) == air {
-		// check down to the right
-		return g.addSand(y1, x+1)
-	}
-
-	// it has come to rest
-	g.mats[y][x] = sand
-	return true
-}
-
-func (g *grid) check(y, x int) material {
-	return g.mats[y][x]
+	return p.rollNorth(), nil
 }
--- puzzles/fourteen/two.go
+++ puzzles/fourteen/two.go
@@ -3,20 +3,5 @@ package fourteen
 func Two(
 	input string,
 ) (int, error) {
-	g, err := getGrid(input)
-	if err != nil {
-		return 0, err
-	}
-
-	g.addFloor()
-
-	units := 0
-	for g.addSand(0, 500) {
-		units++
-		if g.check(0, 500) == sand {
-			break
-		}
-	}
-
-	return units, nil
+	return 0, nil
 }
