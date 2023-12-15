3fc24d4ce7a0ba2525df4c0adace84f3047dd2b9
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-15 12:12:49 +0000

    solve 15 pt 1
    first try

--- puzzles/fifteen/one.go
+++ puzzles/fifteen/one.go
@@ -3,58 +3,22 @@ package fifteen
 func One(
 	input string,
 ) (int, error) {
-	rs, err := getReports(input)
-	if err != nil {
-		return 0, err
+	total := 0
+	var cur uint8
+	for len(input) > 0 {
+		switch input[0] {
+		case ',':
+			total += int(cur)
+			cur = 0
+		case '\n':
+			// do nothing
+		default:
+			cur += uint8(input[0])
+			cur *= 17
+		}
+		input = input[1:]
 	}
-
-	ts := newTuples(len(rs))
-
-	var x1, x2 int
-	var ok bool
-
-	for _, r := range rs {
-		x1, x2, ok = r.getSeenInRow(2000000)
-		if ok {
-			ts.add(x1, x2)
-		}
-	}
-
-	// record known x positions for beacons in this row
-	xs := make([]int, 0, len(rs))
-	contains := false
-	for _, r := range rs {
-		if r.by != 2000000 {
-			continue
-		}
-		for _, x := range xs {
-			if x == r.bx {
-				contains = true
-				break
-			}
-		}
-		if !contains {
-			xs = append(xs, r.bx)
-		}
-	}
-
-	output := make([]tuple, 0, len(rs))
-	output = ts.populate(output)
-
-	// total starts at len(output) instead of zero because every element will add
-	// (t2 - t1 + 1) to the total
-	total := len(output)
-
-	for _, t := range output {
-		total += t.t2 - t.t1
-
-		// remove any known beacons in this range
-		for _, x := range xs {
-			if t.t1 <= x && x <= t.t2 {
-				total--
-			}
-		}
-	}
+	total += int(cur)
 
 	return total, nil
 }
--- puzzles/fifteen/two.go
+++ puzzles/fifteen/two.go
@@ -1,70 +1,7 @@
 package fifteen
 
-import (
-	"errors"
-)
-
 func Two(
 	input string,
 ) (int, error) {
-	const (
-		max = 4000000
-	)
-
-	rs, err := getReports(input)
-	if err != nil {
-		return 0, err
-	}
-
-	var x1, x2 int
-	var ok bool
-	var start [numReports]int
-	var spans [numReports]tuple
-	for i := range spans {
-		x1, x2, ok = rs[i].getSeenInRow(0)
-		if ok {
-			spans[i].t1 = x1
-			spans[i].t2 = x2
-		} else {
-			spans[i].t2 = -1
-			start[i] = rs[i].sy - rs[i].dist
-		}
-	}
-
-	ts := newTuples(len(rs))
-
-	var gap int
-	var i int
-
-	for y := 0; y <= max; y++ {
-		for i = range spans {
-			if spans[i].t2 < spans[i].t1 {
-				if y == start[i] {
-					x1, x2, _ = rs[i].getSeenInRow(y)
-					spans[i].t1 = x1
-					spans[i].t2 = x2
-					ts.add(spans[i].t1, spans[i].t2)
-				}
-			} else {
-				if y <= rs[i].sy {
-					spans[i].t1--
-					spans[i].t2++
-					ts.add(spans[i].t1, spans[i].t2)
-				} else if spans[i].t1 == spans[i].t2 {
-					spans[i].t1 = 1
-					spans[i].t2 = 0
-				} else {
-					spans[i].t1++
-					spans[i].t2--
-					ts.add(spans[i].t1, spans[i].t2)
-				}
-			}
-		}
-		gap = ts.findGap(0, max)
-		if gap >= 0 {
-			return gap*max + y, nil
-		}
-	}
-
-	return 0, errors.New("not found")
+	return 0, nil
 }
