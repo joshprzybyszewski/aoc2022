65814adf26316afe8db1413b10928cc7412da52a
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-16 13:38:56 +0000

    solve pt 2

--- puzzles/sixteen/one.go
+++ puzzles/sixteen/one.go
@@ -1,102 +1,179 @@
 package sixteen
 
-import (
-	"runtime"
-	"sync"
-)
-
 const (
-	// This is assumed in the puzzle.
-	startingNode = `AA`
+	size = 110
 )
 
 func One(
 	input string,
 ) (int, error) {
+	c := newContraption(input)
+	c.sendLight(coord{
+		row: 0,
+		col: 0,
+	}, right)
 
-	valves, err := getValves(input)
-	if err != nil {
-		return 0, err
-	}
-	g := buildGraph(startingNode, &valves)
-
-	return getBestPath(
-		&g,
-		30,
-	), nil
+	return c.numEnergized(), nil
 }
 
-func getBestPath(
-	g *graph,
-	remaining distance,
-) int {
+type coord struct {
+	row uint8
+	col uint8
+}
 
-	var wg sync.WaitGroup
-	var best soloPath
-	var bestLock sync.RWMutex
-	checkBest := func(o soloPath) {
-		bestLock.Lock()
-		defer bestLock.Unlock()
-		if o.released > best.released {
-			best = o
+type tile uint8
+
+const (
+	empty     tile = 0
+	splitUp   tile = 1 << 0
+	splitWide tile = 1 << 1
+	reflect1  tile = 1 << 2 // /
+	reflect2  tile = 1 << 3 // \
+)
+
+type lightTile uint8
+
+const (
+	left  lightTile = 1 << 0
+	right lightTile = 1 << 1
+	up    lightTile = 1 << 2
+	down  lightTile = 1 << 3
+)
+
+func (lt lightTile) String() string {
+	switch lt {
+	case left:
+		return `left`
+	case right:
+		return `right`
+	case up:
+		return `up`
+	case down:
+		return `down`
+	}
+	return `none`
+}
+
+func (lt lightTile) update(
+	c coord,
+	t tile,
+) ([2]coord, [2]lightTile) {
+	sendRight := false
+	sendLeft := false
+	sendDown := false
+	sendUp := false
+
+	switch t {
+	case empty:
+		sendRight = lt == right
+		sendLeft = lt == left
+		sendDown = lt == down
+		sendUp = lt == up
+	case splitUp:
+		sendDown = lt&(down|left|right) == lt
+		sendUp = lt&(up|left|right) == lt
+	case splitWide:
+		sendLeft = lt&(up|down|left) == lt
+		sendRight = lt&(up|down|right) == lt
+	case reflect1:
+		sendRight = lt == up
+		sendLeft = lt == down
+		sendDown = lt == left
+		sendUp = lt == right
+	case reflect2:
+		sendRight = lt == down
+		sendLeft = lt == up
+		sendDown = lt == right
+		sendUp = lt == left
+
+	}
+
+	outI := 0
+	outCoords := [2]coord{}
+	outDirs := [2]lightTile{}
+
+	if sendRight && c.col < size-1 {
+		outCoords[outI] = c
+		outCoords[outI].col++
+		outDirs[outI] = right
+		outI++
+	}
+	if sendLeft && c.col > 0 {
+		outCoords[outI] = c
+		outCoords[outI].col--
+		outDirs[outI] = left
+		outI++
+	}
+	if sendDown && c.row < size-1 {
+		outCoords[outI] = c
+		outCoords[outI].row++
+		outDirs[outI] = down
+		outI++
+	}
+	if sendUp && c.row > 0 {
+		outCoords[outI] = c
+		outCoords[outI].row--
+		outDirs[outI] = up
+		outI++
+	}
+	return outCoords, outDirs
+}
+
+type contraption struct {
+	tiles [size][size]tile
+
+	energized [size][size]lightTile
+}
+
+func newContraption(input string) contraption {
+	ri, ci := 0, 0
+	c := contraption{}
+	for len(input) > 0 {
+		switch input[0] {
+		case '.':
+			c.tiles[ri][ci] = empty
+		case '|':
+			c.tiles[ri][ci] = splitUp
+		case '-':
+			c.tiles[ri][ci] = splitWide
+		case '/':
+			c.tiles[ri][ci] = reflect1
+		case '\\':
+			c.tiles[ri][ci] = reflect2
+		case '\n':
+			ri++
+			ci = -1
+		}
+		ci++
+		input = input[1:]
+	}
+
+	return c
+}
+
+func (c *contraption) sendLight(
+	co coord,
+	lt lightTile,
+) {
+	if lt == 0 || (c.energized[co.row][co.col])&lt == lt {
+		// already energed in this way
+		return
+	}
+	c.energized[co.row][co.col] |= lt
+
+	cs, lts := lt.update(co, c.tiles[co.row][co.col])
+	c.sendLight(cs[0], lts[0])
+	c.sendLight(cs[1], lts[1])
+}
+
+func (c *contraption) numEnergized() int {
+	total := 0
+	for ri := 0; ri < size; ri++ {
+		for ci := 0; ci < size; ci++ {
+			if c.energized[ri][ci] != 0 {
+				total++
+			}
 		}
 	}
-
-	canBeatBest := func(s soloPath) bool {
-		bestLock.RLock()
-		br := best.released
-		bestLock.RUnlock()
-
-		rem := pressure(s.remaining)
-		potRel := s.released
-
-		for n := node(0); n < numNodes; n++ {
-			if s.isOpen(n) {
-				continue
-			}
-			rem--
-			potRel += rem * pressure(g.getValue(n))
-			if potRel > br {
-				return true
-			}
-			rem--
-			if rem < 0 {
-				break
-			}
-		}
-
-		return false
-	}
-
-	work := make(chan int, numNodes)
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go func() {
-			var em soloPath
-			var d distance
-			for w := range work {
-
-				d = g.startingPositions[w]
-				em = maximize(
-					g,
-					soloPath{
-						cur:       node(w),
-						remaining: remaining - d,
-					},
-					canBeatBest,
-				)
-				checkBest(em)
-				wg.Done()
-			}
-		}()
-	}
-
-	for n1 := range g.startingPositions {
-		wg.Add(1)
-		work <- n1
-	}
-
-	wg.Wait()
-	close(work)
-
-	return int(best.released)
+	return total
 }
