f3d13ef687a007193bc7549b2424fbfd46cfb649
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-16 13:47:38 +0000

    micro optimize

--- puzzles/sixteen/one.go
+++ puzzles/sixteen/one.go
@@ -40,85 +40,6 @@ const (
 	down  lightTile = 1 << 3
 )
 
-func (lt lightTile) String() string {
-	switch lt {
-	case left:
-		return `left`
-	case right:
-		return `right`
-	case up:
-		return `up`
-	case down:
-		return `down`
-	}
-	return `none`
-}
-
-func (lt lightTile) update(
-	c coord,
-	t tile,
-) ([2]coord, [2]lightTile) {
-	sendRight := false
-	sendLeft := false
-	sendDown := false
-	sendUp := false
-
-	switch t {
-	case empty:
-		sendRight = lt == right
-		sendLeft = lt == left
-		sendDown = lt == down
-		sendUp = lt == up
-	case splitUp:
-		sendDown = lt&(down|left|right) == lt
-		sendUp = lt&(up|left|right) == lt
-	case splitWide:
-		sendLeft = lt&(up|down|left) == lt
-		sendRight = lt&(up|down|right) == lt
-	case reflect1:
-		sendRight = lt == up
-		sendLeft = lt == down
-		sendDown = lt == left
-		sendUp = lt == right
-	case reflect2:
-		sendRight = lt == down
-		sendLeft = lt == up
-		sendDown = lt == right
-		sendUp = lt == left
-
-	}
-
-	outI := 0
-	outCoords := [2]coord{}
-	outDirs := [2]lightTile{}
-
-	if sendRight && c.col < size-1 {
-		outCoords[outI] = c
-		outCoords[outI].col++
-		outDirs[outI] = right
-		outI++
-	}
-	if sendLeft && c.col > 0 {
-		outCoords[outI] = c
-		outCoords[outI].col--
-		outDirs[outI] = left
-		outI++
-	}
-	if sendDown && c.row < size-1 {
-		outCoords[outI] = c
-		outCoords[outI].row++
-		outDirs[outI] = down
-		outI++
-	}
-	if sendUp && c.row > 0 {
-		outCoords[outI] = c
-		outCoords[outI].row--
-		outDirs[outI] = up
-		outI++
-	}
-	return outCoords, outDirs
-}
-
 type contraption struct {
 	tiles [size][size]tile
 
@@ -130,16 +51,16 @@ func newContraption(input string) contraption {
 	c := contraption{}
 	for len(input) > 0 {
 		switch input[0] {
-		case '.':
+		case '.': // 10927
 			c.tiles[ri][ci] = empty
-		case '|':
-			c.tiles[ri][ci] = splitUp
-		case '-':
-			c.tiles[ri][ci] = splitWide
-		case '/':
-			c.tiles[ri][ci] = reflect1
-		case '\\':
+		case '\\': // 306
 			c.tiles[ri][ci] = reflect2
+		case '/': // 301
+			c.tiles[ri][ci] = reflect1
+		case '-': // 289
+			c.tiles[ri][ci] = splitWide
+		case '|': // 277
+			c.tiles[ri][ci] = splitUp
 		case '\n':
 			ri++
 			ci = -1
@@ -155,21 +76,68 @@ func (c *contraption) sendLight(
 	co coord,
 	lt lightTile,
 ) {
-	if lt == 0 || (c.energized[co.row][co.col])&lt == lt {
+	if (c.energized[co.row][co.col])&lt == lt {
 		// already energed in this way
 		return
 	}
 	c.energized[co.row][co.col] |= lt
 
-	cs, lts := lt.update(co, c.tiles[co.row][co.col])
-	c.sendLight(cs[0], lts[0])
-	c.sendLight(cs[1], lts[1])
+	sendRight := false
+	sendLeft := false
+	sendDown := false
+	sendUp := false
+
+	switch c.tiles[co.row][co.col] {
+	case empty:
+		sendRight = lt == right
+		sendLeft = lt == left
+		sendDown = lt == down
+		sendUp = lt == up
+	case reflect2:
+		sendRight = lt == down
+		sendLeft = lt == up
+		sendDown = lt == right
+		sendUp = lt == left
+	case reflect1:
+		sendRight = lt == up
+		sendLeft = lt == down
+		sendDown = lt == left
+		sendUp = lt == right
+	case splitWide:
+		sendLeft = lt&(up|down|left) == lt
+		sendRight = lt&(up|down|right) == lt
+	case splitUp:
+		sendDown = lt&(down|left|right) == lt
+		sendUp = lt&(up|left|right) == lt
+	}
+
+	if sendRight && co.col < size-1 {
+		tmp := co
+		tmp.col++
+		c.sendLight(tmp, right)
+	}
+	if sendLeft && co.col > 0 {
+		tmp := co
+		tmp.col--
+		c.sendLight(tmp, left)
+	}
+	if sendDown && co.row < size-1 {
+		tmp := co
+		tmp.row++
+		c.sendLight(tmp, down)
+	}
+	if sendUp && co.row > 0 {
+		tmp := co
+		tmp.row--
+		c.sendLight(tmp, up)
+	}
 }
 
 func (c *contraption) numEnergized() int {
 	total := 0
+	var ci int
 	for ri := 0; ri < size; ri++ {
-		for ci := 0; ci < size; ci++ {
+		for ci = 0; ci < size; ci++ {
 			if c.energized[ri][ci] != 0 {
 				total++
 			}
