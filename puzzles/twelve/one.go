97c16ebebd10b858f88ce7244c1b25c62ccdd0fe
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-16 23:06:26 +0000

    this doesn't work

--- puzzles/twelve/one.go
+++ puzzles/twelve/one.go
@@ -37,187 +37,119 @@ func (r row) String() string {
 	return sb.String()
 }
 
-func (r row) isSolution(indexes []int) bool {
-	ii := 0
-	cur := 0
-	for i := 0; i < len(r.parts); i++ {
-		if r.parts[i] == broken {
-			cur++
-		} else if cur > 0 {
-			if ii >= len(indexes) || cur != indexes[ii] {
-				return false
-			}
-			ii++
-			cur = 0
-		}
-	}
-	if ii < len(indexes) {
-		if cur != indexes[ii] {
-			return false
-		}
-		cur = 0
-		ii++
-	}
-	return ii == len(indexes) && cur == 0
-}
-
-func (r row) getPossibilities(indexes []int) int {
-	// fmt.Printf("-- %s %v\n", r, indexes)
-	total := solveNext(r, 0, indexes)
-	// fmt.Printf("   %d\n", total)
-	return total
-}
-
-func solveNext(
-	r row,
-	i int,
-	indexes []int,
-) int {
-	for i < len(r.parts) && r.parts[i] != unknown {
-		i++
-	}
-
-	if i >= len(r.parts) {
-		if r.isSolution(indexes) {
-			// fmt.Printf("   %s\n", r)
-			return 1
-		}
-		return 0
-	}
-
-	r1 := r
-	r1.parts[i] = broken
-	r.parts[i] = safe
-	i++
-
-	return solveNext(r1, i, indexes) + solveNext(r, i, indexes)
-}
-
-func getNumConfigurations(line string) int {
-	var r row
-	var i int
-	for i = 0; i < len(line); i++ {
-		if line[i] == ' ' {
-			r.numParts = i
-			i++
-			break
-		}
-		switch line[i] {
-		case '?':
-			r.parts[i] = unknown
-		case '#':
-			r.parts[i] = broken
-		}
-	}
-
-	curNum := 0
-	var indexes []int
-	for ; i < len(line); i++ {
-		if line[i] == ',' {
-			indexes = append(indexes, curNum)
-			curNum = 0
-			continue
-		}
-		curNum *= 10
-		curNum += int(line[i] - '0')
-	}
-	indexes = append(indexes, curNum)
-
-	return r.getPossibilitiesV2(indexes)
-}
-
 func One(
 	input string,
 ) (int, error) {
 	total := 0
-	for nli := strings.Index(input, "\n"); nli >= 0; nli = strings.Index(input, "\n") {
-		total += getNumConfigurations(input[:nli])
-		input = input[nli+1:]
+	var i int
+	var r row
+	var addGroup bool
+	groups := make([]int, 0, 8)
+	for len(input) > 0 {
+		if input[0] == '\n' {
+			if r.numParts > 0 {
+				num := getNum(r, 0, groups)
+				// fmt.Printf("%20s %v := %d\n", r, groups, num)
+				total += num
+			}
+			i = 0
+			r = row{}
+			addGroup = false
+			groups = groups[:0]
+		} else if addGroup {
+			switch input[0] {
+			case ',':
+				// iterate past.
+			default:
+				groups = append(groups, int(input[0]-'0'))
+			}
+		} else {
+			switch input[0] {
+			case '?':
+				r.parts[i] = unknown
+			case '#':
+				r.parts[i] = broken
+			case ' ':
+				r.numParts = i
+				addGroup = true
+			}
+			i++
+		}
+		input = input[1:]
 	}
 	return total, nil
 }
 
-func (r row) getPossibilitiesV2(
-	busted []int,
+func getNum(
+	r row,
+	start int,
+	groups []int,
 ) int {
-
-	maxToSkip := r.numParts + 1 - len(busted)
-	for bi := range busted {
-		maxToSkip -= busted[bi]
+	// fmt.Printf("  %20s %v\tstart = %2d\n", r, groups, start)
+	if len(groups) == 0 {
+		panic(`unexpected`)
 	}
 
 	total := 0
-
-	for toSkip := 0; toSkip <= maxToSkip; toSkip++ {
-		total += r.getPossibilitiesV2_recursive(0, toSkip, busted)
+	maxI := r.numParts
+	if len(groups) > 0 {
+		maxI -= (len(groups) - 1)
+	}
+	for _, g := range groups {
+		maxI -= g
+	}
+	for i := start; i <= maxI; i++ {
+		if i > 0 && r.parts[i-1] == broken {
+			// TODO this can be faster
+			break
+		}
+		if !canPlace(r, i, groups[0]) {
+			continue
+		}
+		if len(groups) == 1 {
+			if hasBroken(r, i+groups[0]+1) {
+				break
+			}
+			total++
+			// fmt.Printf("   works!\n")
+		} else {
+			rCpy := r
+			for j := i; j < i+groups[0]; j++ {
+				rCpy.parts[j] = broken
+			}
+			rCpy.parts[i+groups[0]] = safe
+			total += getNum(rCpy, i+groups[0]+1, groups[1:])
+		}
 	}
 
 	return total
 }
 
-func (r row) getPossibilitiesV2_recursive(
-	minUncheckedIndex int,
-	toSkip int,
-	remainingBusted []int,
-) int {
-	if !r.isPossibleV2(minUncheckedIndex, toSkip, remainingBusted) {
-		return 0
-	}
-
-	minUncheckedIndex += toSkip + remainingBusted[0]
-	remainingBusted = remainingBusted[1:]
-
-	if len(remainingBusted) == 0 {
-		for ; minUncheckedIndex < r.numParts; minUncheckedIndex++ {
-			if r.parts[minUncheckedIndex] == broken {
-				return 0
-			}
-		}
-		return 1
-	}
-
-	numPossible := 0
-	maxToSkip := r.numParts - minUncheckedIndex - len(remainingBusted)
-	for toSkip = 1; toSkip <= maxToSkip; toSkip++ {
-		numPossible += r.getPossibilitiesV2_recursive(
-			minUncheckedIndex,
-			toSkip,
-			remainingBusted,
-		)
-	}
-	return numPossible
-}
-
-func (r row) isPossibleV2(
-	minUncheckedIndex int,
-	toSkip int,
-	remainingBusted []int,
+func canPlace(
+	r row,
+	start int,
+	group int,
 ) bool {
-
-	var n int
-
-	i := minUncheckedIndex
-
-	if i+toSkip+remainingBusted[0] > r.numParts {
+	maxI := start + group
+	if maxI > r.numParts {
 		return false
 	}
-	for n = 0; n < toSkip; n++ {
-		if r.parts[i] == broken {
-			return false
-		}
-		i++
-	}
-
-	for n = 0; n < remainingBusted[0]; n++ {
+	for i := start; i < maxI; i++ {
 		if r.parts[i] == safe {
 			return false
 		}
-		i++
 	}
+	return maxI == r.numParts || r.parts[maxI] != broken
+}
 
-	for n = 1; n < len(remainingBusted); n++ {
-		i += 1 + remainingBusted[n]
+func hasBroken(
+	r row,
+	start int,
+) bool {
+	for i := start; i < r.numParts; i++ {
+		if r.parts[i] == broken {
+			return true
+		}
 	}
-
-	return i <= r.numParts
+	return false
 }
