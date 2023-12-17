72ef888b0d1b8b0c60b3a0f2f7fd8cfa9cec59b1
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-17 17:04:34 +0000

    first attempt failed

--- puzzles/seventeen/one.go
+++ puzzles/seventeen/one.go
@@ -1,7 +1,9 @@
 package seventeen
 
 import (
+	"fmt"
 	"slices"
+	"strings"
 )
 
 const (
@@ -14,11 +16,16 @@ func One(
 	input string,
 ) (int, error) {
 	c := newCity(input)
-	return 0, nil
+	dijkstraHeatLossToTarget(&c)
+	fmt.Printf("city:\n%s\n", c)
+	// 1356 is too high
+	return getMinimalHeatLoss(c), nil
 }
 
 type city struct {
 	blocks [citySize][citySize]int
+
+	minHeatLossToTarget [citySize][citySize]int
 }
 
 func newCity(input string) city {
@@ -36,6 +43,62 @@ func newCity(input string) city {
 	}
 
 	return c
+}
+
+func (c city) String() string {
+	var sb strings.Builder
+	for ri := 0; ri < citySize; ri++ {
+		for ci := 0; ci < citySize; ci++ {
+			sb.WriteString(fmt.Sprintf("%4d ", c.blocks[ri][ci]))
+		}
+		sb.WriteByte('\n')
+	}
+	return sb.String()
+}
+
+func dijkstraHeatLossToTarget(c *city) {
+	pending := make([]position, 0, 128)
+
+	pending = append(pending,
+		position{
+			row:           citySize - 2,
+			col:           citySize - 1,
+			totalHeatLoss: c.blocks[citySize-1][citySize-1],
+		},
+		position{
+			row:           citySize - 1,
+			col:           citySize - 2,
+			totalHeatLoss: c.blocks[citySize-1][citySize-1],
+		},
+	)
+
+	for len(pending) > 0 {
+		pos := pending[0]
+		if pos.row < 0 || pos.col < 0 {
+			pending = pending[1:]
+			continue
+		}
+
+		if c.minHeatLossToTarget[pos.row][pos.col] == 0 ||
+			c.minHeatLossToTarget[pos.row][pos.col] > pos.totalHeatLoss {
+			// if it's unset or it's more than we currently know, set it
+			c.minHeatLossToTarget[pos.row][pos.col] = pos.totalHeatLoss
+			pending = append(pending,
+				position{
+					row:           pos.row - 1,
+					col:           pos.col,
+					totalHeatLoss: pos.totalHeatLoss + c.blocks[pos.row][pos.col],
+				},
+				position{
+					row:           pos.row,
+					col:           pos.col - 1,
+					totalHeatLoss: pos.totalHeatLoss + c.blocks[pos.row][pos.col],
+				},
+			)
+		}
+
+		pending = pending[1:]
+	}
 }
 
 type heading uint8
@@ -57,6 +120,10 @@ type position struct {
 	numInDirection int
 
 	totalHeatLoss int
+}
+
+func (p position) String() string {
+	return fmt.Sprintf("(%3d, %3d) %d", p.row, p.col, p.totalHeatLoss)
 }
 
 func getMinimalHeatLoss(
@@ -90,6 +157,12 @@ func getMinimalHeatLoss(
 		pending.sort()
 		pos = pending.pop()
 
+		fmt.Printf("Processing: %s\n", pos)
+
+		if pending.checkSolution(pos) {
+			break
+		}
+
 		left, straight, right = getNext(c, pos)
 		if left != nil {
 			pending.insert(*left)
@@ -100,10 +173,9 @@ func getMinimalHeatLoss(
 		if right != nil {
 			pending.insert(*right)
 		}
-
 	}
 
-	return 0
+	return pending.best.totalHeatLoss
 }
 
 func getNext(
@@ -137,17 +209,87 @@ func getNext(
 				straight = nil
 			}
 		}
-
-		straight = &s
 	}
 
-	// TODO populate left and right
+	l := pos
+	left = &l
+	l.numInDirection = 0
 
-	return nil, straight, nil
+	switch l.lastHeading {
+	case south:
+		l.lastHeading = east
+		l.col++
+		if l.col >= citySize {
+			left = nil
+		}
+	case west:
+		l.lastHeading = south
+		l.row++
+		if l.row >= citySize {
+			left = nil
+		}
+	case north:
+		l.lastHeading = west
+		l.col--
+		if l.col < 0 {
+			left = nil
+		}
+	case east:
+		l.lastHeading = north
+		l.row--
+		if l.row < 0 {
+			left = nil
+		}
+	}
+
+	r := pos
+	right = &r
+	r.numInDirection = 0
+
+	switch r.lastHeading {
+	case north:
+		r.lastHeading = east
+		r.col++
+		if r.col >= citySize {
+			right = nil
+		}
+	case east:
+		r.lastHeading = south
+		r.row++
+		if r.row >= citySize {
+			right = nil
+		}
+	case south:
+		r.lastHeading = west
+		r.col--
+		if r.col < 0 {
+			right = nil
+		}
+	case west:
+		r.lastHeading = north
+		r.row--
+		if r.row < 0 {
+			right = nil
+		}
+	}
+
+	if left != nil {
+		left.totalHeatLoss += c.blocks[left.row][left.col]
+	}
+	if straight != nil {
+		straight.totalHeatLoss += c.blocks[straight.row][straight.col]
+	}
+	if right != nil {
+		right.totalHeatLoss += c.blocks[right.row][right.col]
+	}
+
+	return left, straight, right
 }
 
 type pending struct {
 	all []position
+
+	best position
 }
 
 func newPending() *pending {
@@ -155,8 +297,23 @@ func newPending() *pending {
 		all: make([]position, 0, 128),
 	}
 }
+
 func (p *pending) isEmpty() bool {
 	return len(p.all) == 0
+}
+
+func (p *pending) checkSolution(
+	pos position,
+) bool {
+	if pos.row != citySize-1 || pos.col != citySize-1 {
+		return false
+	}
+
+	// TODO check if pos.heatLoss < p.best.heatLoss
+	p.best = pos
+
+	// TODO filter out
+	return true
 }
 
 func (p *pending) insert(
@@ -173,10 +330,6 @@ func (p *pending) sort() {
 func comparePositions(
 	a, b position,
 ) int {
-	// adist := (citySize - 1 - a.row) + (citySize - 1 - a.col)
-	// bdist := (citySize - 1 - b.row) + (citySize - 1 - b.col)
-	// adist := 2*citySize - 2 - a.row - a.col
-	// bdist := 2*citySize - 2 - b.row - b.col
 	adist := a.row + a.col
 	bdist := b.row + b.col
 	if adist != bdist {
