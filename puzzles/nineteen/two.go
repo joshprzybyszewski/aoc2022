b2a7a125d9a469f8a6f06d62f0903374af0bddb0
Joshua Przybyszewski <josh.prezby@gmail.com>
2023-12-19 13:31:51 +0000

    solve 19 pt 1

--- puzzles/nineteen/two.go
+++ puzzles/nineteen/two.go
@@ -1,41 +1,7 @@
 package nineteen
 
-import (
-	"runtime"
-	"sync"
-)
-
-const (
-	part2Minutes = 32
-)
-
 func Two(
 	input string,
 ) (int, error) {
-
-	all, err := getBlueprints(input)
-	if err != nil {
-		return 0, err
-	}
-
-	var vals [3]int
-	var wg sync.WaitGroup
-	work := make(chan int, len(vals))
-	for i := 0; i < runtime.NumCPU(); i++ {
-		go func() {
-			for w := range work {
-				vals[w] = harvest(&all[w], part2Minutes)
-				wg.Done()
-			}
-		}()
-	}
-
-	for i := 0; i < len(vals); i++ {
-		wg.Add(1)
-		work <- i
-	}
-
-	wg.Wait()
-
-	return vals[0] * vals[1] * vals[2], nil
+	return 0, nil
 }
