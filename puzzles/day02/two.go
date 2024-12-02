eb505057347c73df52046f93ed838a4ac133cb5d
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-02 08:08:00 -0600

    brute force part 2

--- puzzles/day02/two.go
+++ puzzles/day02/two.go
@@ -1,55 +1,81 @@
 package day02
 
 import (
-	"strings"
-
 	"github.com/joshprzybyszewski/aoc2022/util/lines"
+	"github.com/joshprzybyszewski/aoc2022/util/strutil"
 )
 
 func Two(
 	input string,
 ) (int, error) {
 
-	sum := 0
-	var isFirst bool
-	var hi int
-	var handful, min handful
-
+	numSafe := 0
+	var l, n int
+	vals := make([]int, 0, 12)
 	lines.ForEach(input, func(line string) {
-		isFirst = true
-
-		line = line[strings.Index(line, `:`)+1:]
+		vals = vals[:0]
 		for {
-			hi = strings.Index(line, semicolon)
-			if hi == -1 {
-				hi = len(line)
-			}
-
-			handful = interpretSeen(line[:hi])
-			if isFirst {
-				min = handful
-				isFirst = false
-			} else {
-				if handful.red > min.red {
-					min.red = handful.red
-				}
-				if handful.green > min.green {
-					min.green = handful.green
-				}
-				if handful.blue > min.blue {
-					min.blue = handful.blue
-				}
-			}
-
-			if hi == len(line) {
+			l, n = strutil.IntBeforeSpace(line)
+			vals = append(vals, l)
+			if n >= len(line) {
 				break
 			}
-
-			line = line[hi+1:]
+			line = line[n+1:]
 		}
-
-		sum += (min.red * min.blue * min.green)
+		if isPart2Safe(vals) {
+			numSafe++
+		}
 	})
 
-	return sum, nil
+	return numSafe, nil
+}
+
+func abs(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func isPart2Safe(vals []int) bool {
+	if isPart1Safe(vals) {
+		return true
+	}
+	valsCpy := make([]int, len(vals)-1)
+	place := func(skip int) {
+		copy(valsCpy, vals[:skip])
+		copy(valsCpy[skip:], vals[skip+1:])
+	}
+	for i := 0; i < len(vals); i++ {
+		place(i)
+		if isPart1Safe(valsCpy) {
+			return true
+		}
+	}
+	return false
+}
+
+func isPart1Safe(vals []int) bool {
+	if len(vals) < 2 {
+		return true
+	}
+	if vals[1] > vals[0] {
+		for i := 1; i < len(vals); i++ {
+			if vals[i] <= vals[i-1] || vals[i] > vals[i-1]+3 {
+				// isSafe = false
+				return false
+			}
+		}
+		return true
+	}
+	if vals[1] == vals[0] {
+		return false
+	}
+	for i := 1; i < len(vals); i++ {
+		if vals[i] >= vals[i-1] || vals[i] < vals[i-1]-3 {
+			// isSafe = false
+			return false
+		}
+	}
+	return true
 }
