122540e16f84d3cfc40d2163c907a0a08f07a0b4
Josh Przybyszewski <josh.prezby@gmail.com>
2024-12-04 05:22:30 -0600

    solve day 4 pt 1

--- puzzles/day04/one.go
+++ puzzles/day04/one.go
@@ -1,111 +1,181 @@
 package day04
 
-import (
-	"strings"
-
-	"github.com/joshprzybyszewski/aoc2022/util/lines"
+const (
+	GRID_SIZE = 140
 )
 
-type card struct {
-	winners [2]uint64
-	shown   [25]int
+const (
+	X_SET   uint8 = 1 << 0
+	M_SET   uint8 = 1 << 1
+	A_SET   uint8 = 1 << 2
+	S_SET   uint8 = 1 << 3
+	ALL_SET uint8 = X_SET | M_SET | A_SET | S_SET
+)
+
+const (
+	right uint8 = 0
+	ur    uint8 = 1
+	up    uint8 = 2
+	ul    uint8 = 3
+	left  uint8 = 4
+	dl    uint8 = 5
+	down  uint8 = 6
+	dr    uint8 = 7
+)
+
+type coord struct {
+	dirs [8]uint8
 }
 
-func newCard(input string) card {
-	tmpi := 0
-	tmpVal := 0
-	pi := strings.Index(input, "|")
-	c := card{}
-	for tmpi = strings.Index(input, ":") + 1; tmpi < pi; tmpi++ {
-		if input[tmpi] == ' ' {
-			if tmpVal != 0 {
-				c = c.addWinner(tmpVal)
-			}
-			tmpVal = 0
-			continue
-		}
-		tmpVal *= 10
-		tmpVal += int(input[tmpi] - '0')
-	}
-	if tmpVal != 0 {
-		c = c.addWinner(tmpVal)
-	}
-
-	tmpi = pi + 1
-	tmpVal = 0
-	pi = 0
-	for ; tmpi < len(input); tmpi++ {
-		if input[tmpi] == ' ' {
-			if tmpVal != 0 {
-				c.shown[pi] = tmpVal
-				pi++
-			}
-			tmpVal = 0
-			continue
-		}
-		tmpVal *= 10
-		tmpVal += int(input[tmpi] - '0')
-	}
-	if tmpVal != 0 {
-		c.shown[pi] = tmpVal
-	}
-
-	return c
+func (c *coord) setX(i uint8) {
+	c.dirs[i] |= X_SET
 }
 
-func (c card) addWinner(w int) card {
-	if w >= 64 {
-		c.winners[1] |= 1 << (w - 64)
-	} else {
-		c.winners[0] |= 1 << w
-	}
-	return c
+func (c *coord) setM(i uint8) {
+	c.dirs[i] |= M_SET
 }
 
-func (c card) isWinner(s int) bool {
-	if s >= 64 {
-		return c.winners[1]&(1<<(s-64)) != 0
-	}
-	return c.winners[0]&(1<<s) != 0
+func (c *coord) setA(i uint8) {
+	c.dirs[i] |= A_SET
 }
 
-func (c card) numMatching() int {
-	total := 0
-	for _, s := range c.shown {
-		if c.isWinner(s) {
-			total++
-		}
-	}
-	return total
-}
-
-func (c card) value() int {
-	total := 0
-	for _, s := range c.shown {
-		if !c.isWinner(s) {
-			continue
-		}
-		if total == 0 {
-			total = 1
-		} else {
-			total <<= 1
-		}
-	}
-	return total
+func (c *coord) setS(i uint8) {
+	c.dirs[i] |= S_SET
 }
 
 func One(
-	input string,
+	inputString string,
 ) (int, error) {
 
-	total := 0
+	input := []byte(inputString)
+	grid := [GRID_SIZE][GRID_SIZE]coord{}
 
-	lines.ForEach(input, func(line string) {
-		if len(line) == 0 {
-			return
+	var r, c, i int
+	setX := func() {
+		grid[r][c].setX(right)
+		grid[r][c].setX(ur)
+		grid[r][c].setX(up)
+		grid[r][c].setX(ul)
+		grid[r][c].setX(left)
+		grid[r][c].setX(dl)
+		grid[r][c].setX(down)
+		grid[r][c].setX(dr)
+	}
+	setM := func() {
+		if c > 0 {
+			grid[r][c-1].setM(right)
+			if r > 0 {
+				grid[r-1][c-1].setM(dr)
+			}
+			if r < len(grid)-1 {
+				grid[r+1][c-1].setM(ur)
+			}
 		}
-		total += newCard(line).value()
-	})
+		if c < len(grid[r])-1 {
+			grid[r][c+1].setM(left)
+			if r > 0 {
+				grid[r-1][c+1].setM(dl)
+			}
+			if r < len(grid)-1 {
+				grid[r+1][c+1].setM(ul)
+			}
+		}
+		if r > 0 {
+			grid[r-1][c].setM(down)
+		}
+		if r < len(grid)-1 {
+			grid[r+1][c].setM(up)
+		}
+	}
+	setA := func() {
+		if c > 1 {
+			grid[r][c-2].setA(right)
+			if r > 1 {
+				grid[r-2][c-2].setA(dr)
+			}
+			if r < len(grid)-2 {
+				grid[r+2][c-2].setA(ur)
+			}
+		}
+		if c < len(grid[r])-2 {
+			grid[r][c+2].setA(left)
+			if r > 1 {
+				grid[r-2][c+2].setA(dl)
+			}
+			if r < len(grid)-2 {
+				grid[r+2][c+2].setA(ul)
+			}
+		}
+		if r > 1 {
+			grid[r-2][c].setA(down)
+		}
+		if r < len(grid)-2 {
+			grid[r+2][c].setA(up)
+		}
+	}
+	setS := func() {
+		if c > 2 {
+			grid[r][c-3].setS(right)
+			if r > 2 {
+				grid[r-3][c-3].setS(dr)
+			}
+			if r < len(grid)-3 {
+				grid[r+3][c-3].setS(ur)
+			}
+		}
+		if c < len(grid[r])-3 {
+			grid[r][c+3].setS(left)
+			if r > 2 {
+				grid[r-3][c+3].setS(dl)
+			}
+			if r < len(grid)-3 {
+				grid[r+3][c+3].setS(ul)
+			}
+		}
+		if r > 2 {
+			grid[r-3][c].setS(down)
+		}
+		if r < len(grid)-3 {
+			grid[r+3][c].setS(up)
+		}
+	}
+
+	var b byte
+	for r = 0; r < len(grid); r++ {
+		for c = 0; c < len(grid[r]); c++ {
+			b = input[i]
+			i++
+			switch b {
+			case 'X':
+				setX()
+			case 'M':
+				setM()
+			case 'A':
+				setA()
+			case 'S':
+				setS()
+				// default:
+				// 	fmt.Printf("Found %q at (%d, %d)\n", string(b), r, c)
+				// 	panic(`ahh` + string(b))
+			}
+		}
+		// if input[i] != '\n' {
+		// 	panic(`ahh`)
+		// }
+		// input[i] should be a newline
+		i++
+	}
+
+	total := 0
+	for r := 0; r < len(grid); r++ {
+		for c = 0; c < len(grid[r]); c++ {
+			for i = 0; i < len(grid[r][c].dirs); i++ {
+				if grid[r][c].dirs[i] == ALL_SET {
+					total++
+				}
+			}
+		}
+	}
 
 	return total, nil
 }
--- puzzles/day04/two.go
+++ puzzles/day04/two.go
@@ -1,34 +1,8 @@
 package day04
 
-import "github.com/joshprzybyszewski/aoc2022/util/lines"
-
 func Two(
 	input string,
 ) (int, error) {
 
-	numCopies := make([]int, 200)
-	total := 0
-
-	tmp, cardCopies, j := 0, 0, 0
-
-	var i int
-
-	lines.ForEach(input, func(line string) {
-		if len(line) == 0 {
-			return
-		}
-
-		// one original copy.
-		cardCopies = numCopies[i] + 1
-		total += cardCopies
-
-		tmp = i + newCard(line).numMatching() + 1
-		for j = i + 1; j < tmp; j++ {
-			numCopies[j] += cardCopies
-		}
-
-		i++
-	})
-
-	return total, nil
+	return 0, nil
 }
